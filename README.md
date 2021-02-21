7c471b10f87d45ee85fe051c4d85772d27500df8
Samantha Brown <71106177+SamMichBrown@users.noreply.github.com>
2021-02-21 17:50:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Note-Taker
+
+## Description
+ An application that allows the user to generate a Work Team for their project using node.js.
+
+## Usage
+To write and take notes and save them in a column based interface for later.
+
+![screencapture-localhost-3001-2021-02-21-14_54_07](https://user-images.githubusercontent.com/71106177/108636727-2d8a1b80-7455-11eb-9c2a-49df8a7824ba.png)
+![screencapture-localhost-3001-notes-2021-02-21-14_57_26](https://user-images.githubusercontent.com/71106177/108636730-2f53df00-7455-11eb-804e-9938b0dfdf8c.png)
+
+## Built With
+
+- JavaScript (ES6)
+- Node.js
+- Inquirer
+- NPM
+- Express
+
+## Website
+ 
+
+### Contribution
+Made by Samantha Brown
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const uniqid = require('uniqid');
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+       if (err) throw err;
+       const notes = JSON.parse(data);
+       res.json(notes);
+    })
+    
+})
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const note = {
+            title: req.body.title,
+            text: req.body.text  
+        }
+        note.id = uniqid();
+        notes.push(note)
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+            console.log("Success!")
+            res.sendFile(path.join(__dirname + '/public/index.html'))
+        })
+        
+     })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id != req.params.id)
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+            console.log('Success!')
+            res.sendFile(path.join(__dirname + '/public/index.html'))
+        })
+        
+     })
+})
+
+app.get('*', (req, res) =>  {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
